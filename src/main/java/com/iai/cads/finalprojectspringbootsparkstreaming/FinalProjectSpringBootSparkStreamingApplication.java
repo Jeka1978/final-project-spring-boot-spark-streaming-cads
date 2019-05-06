45c16ecc4086d92ce21364be9e1edf4903b921ed
jeka1978 <kit2009>
2019-05-06 10:32:55 +0300

    Initial commit

---
+++ src/main/java/com/iai/cads/finalprojectspringbootsparkstreaming/FinalProjectSpringBootSparkStreamingApplication.java
@@ -0,0 +1,13 @@
+package com.iai.cads.finalprojectspringbootsparkstreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectSpringBootSparkStreamingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectSpringBootSparkStreamingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iai/cads/finalprojectspringbootsparkstreaming/FinalProjectSpringBootSparkStreamingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.iai.cads.finalprojectspringbootsparkstreaming;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FinalProjectSpringBootSparkStreamingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
