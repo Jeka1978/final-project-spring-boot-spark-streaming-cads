34d1485868bf3fdb9377fa82ce4900eee515c0b3
jeka1978 <kit2009>
2019-05-22 12:55:16 +0300

    starting with spark streaming DStream from json

--- src/main/scala/spark_streaming/DstreamEnrichmentMain.scala
+++ src/main/scala/spark_streaming/DstreamEnrichmentMain.scala
@@ -2,7 +2,6 @@ package spark_streaming
 
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.{Seconds, StreamingContext}
-import org.codehaus.jackson.map.ObjectMapper
 
 
 /**
@@ -10,14 +9,16 @@ import org.codehaus.jackson.map.ObjectMapper
   */
 object DstreamEnrichmentMain {
   def main(args: Array[String]): Unit = {
-  /*  val conf = new SparkConf().setAppName("test").setMaster("local[*]")
-    val ssc = new StreamingContext(conf,Seconds(10))
-    val dStream = ssc.socketTextStream("localhost",9999)*/
+    val conf = new SparkConf().setAppName("test").setMaster("local[*]")
+    val ssc = new StreamingContext(conf, Seconds(1))
+    val dStream = ssc.socketTextStream("localhost", 9999)
+    dStream
+      .map(Purchase.fromJson)
+      .print()
 
-    val json = "{ \"client_id\": 9, \"cashbox_id\": 3, \"product_id\": 2}"
-    val mapper = new ObjectMapper()
-    val purchase = mapper.readValue(json,classOf[Purchase])
-    println(purchase)
+    ssc.start()
+    ssc.awaitTermination()
+
 
   }
 
--- src/main/scala/spark_streaming/Purchase.scala
+++ src/main/scala/spark_streaming/Purchase.scala
@@ -1,8 +1,17 @@
 package spark_streaming
 
+import org.json4s.DefaultFormats
+import org.json4s.jackson.Serialization.read
+
 /**
   * @author Evgeny Borisov
   */
 case class Purchase(product_id:Int,client_id:Int,cashbox_id:Int) {
 
+}
+object Purchase{
+  implicit val formats: DefaultFormats.type = DefaultFormats
+  def fromJson(json:String):Purchase={
+    read[Purchase](json)
+  }
 }
